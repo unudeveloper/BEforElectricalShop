activeadmin/activeadmin

Fix ActiveAdmin::Inputs::Filters::DateRangeInput cover last day

lib/active_admin/inputs/filters/date_range_input.rb
@@ -20,7 +20,7 @@ module ActiveAdmin
         alias :input_name :gt_input_name
 
         def lt_input_name
-          "#{method}_lteq"
+          "#{method}_lteqdate"
         end
 
         def input_html_options(input_name = gt_input_name)

unudeveloper/BEforElectricalShop

lib/active_admin/inputs/filters/date_range_input.rb
@@ -15,12 +15,12 @@ module ActiveAdmin
         end
 
         def gt_input_name
-          "#{method}_gteq"
+          "#{method}_gteq_date"
         end
         alias :input_name :gt_input_name
 
         def lt_input_name
-          "#{method}_lteq"
+          "#{method}_lteq_date"
         end
 
         def input_html_options(input_name = gt_input_name)